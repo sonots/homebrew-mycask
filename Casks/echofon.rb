96d49cdcefa5c8780b0b5786b6bc2dd33c8d1486
sonots <sonots@gmail.com>
2013-12-28 17:53:42 +0900

    Add Echofon

---
+++ Casks/echofon.rb
@@ -0,0 +1,7 @@
+class Echofon < Cask
+  url 'http://www.echofon.com/bin/Echofon_1.8.0.dmg'
+  homepage 'http://www.echofon.com/twitter/mac/'
+  version '1.8.0'
+  sha1 'e02251fc05847df223295f364b5a004019d47f7b'
+  link 'Echofon.app'
+end
