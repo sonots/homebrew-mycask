7c76b632197a3831db36f54fadc6cd4f922e50fd
sonots <sonots@gmail.com>
2013-12-28 22:08:28 +0900

    add macvim-kaoriya

---
+++ Casks/macvim-kaoriya.rb
@@ -0,0 +1,7 @@
+class MacvimKaoriya < Cask
+  url 'https://macvim-kaoriya.googlecode.com/files/macvim-kaoriya-20131126.dmg'
+  homepage 'https://code.google.com/p/macvim-kaoriya/'
+  version '20131126'
+  sha1 '9908b98f79c5bbaf77f9444958f2416faf6fbf1f'
+  link 'MacVim.app'
+end
