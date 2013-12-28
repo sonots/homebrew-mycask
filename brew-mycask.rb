525348732364fa1010b64882668b9fefc122e43d
sonots <sonots@gmail.com>
2013-12-28 21:13:36 +0900

    add brew-mycask formula

---
+++ brew-mycask.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class BrewMycask < Formula
+  homepage 'https://github.com/sonots/homebrew-mycask/'
+  url 'https://github.com/sonots/homebrew-mycask.git'
+  def install
+  end
+end
