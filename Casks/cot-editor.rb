79e947d55d0629bdadb1740d228f75218c3ffa64
sonots <sonots@gmail.com>
2013-12-28 21:42:27 +0900

    add CotEditor

---
+++ Casks/cot-editor.rb
@@ -0,0 +1,7 @@
+class CotEditor < Cask
+  url 'http://sourceforge.jp/frs/redir.php?m=iij&f=%2Fcoteditor%2F54872%2FCotEditor_1.3.1_For10.7.dmg'
+  homepage 'http://sourceforge.jp/projects/coteditor/'
+  version '1.3.1'
+  md5 '13a7349faf54a30c6d89ac416f392d5c'
+  link 'CotEditor.app'
+end
